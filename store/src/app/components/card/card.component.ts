47f682f433d21321216a4ffbf4ee2ea3b26493b6
Danilo <danilosnt77@gmail.com>
2024-05-21 11:38:13 -0300

    Finish Project

---
+++ store/src/app/components/card/card.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CardLabelComponent } from "./card-label/card-label.component";
+import { CardPriceComponent } from "./card-price/card-price.component";
+
+@Component({
+    selector: 'app-card',
+    standalone: true,
+    templateUrl: './card.component.html',
+    styleUrl: './card.component.css',
+    imports: [CardLabelComponent, CardPriceComponent]
+})
+export class CardComponent implements OnInit {
+
+	@Input()
+	gameCover:string = ""
+	@Input()
+	gameLabel:string=""
+	@Input()
+	gameType:string ="XPTO | PS4"
+	@Input()
+	gamePrice:string = "R$ 399,90"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
